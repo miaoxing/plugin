107e99ca1bd670618d579c1a777c138c156880c4
twin <twinhuang@qq.com>
2016-08-12 15:04:00 +0800

    增加travis配置支持 #1504

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,13 @@ $files = [
     'tests/config-local.php'
 ];
 
+// Add configuration file for CI
+foreach (array('TRAVIS') as $ci) {
+    if (getenv($ci)) {
+        $files[] = 'config-' . strtolower($ci) . '.php';
+    }
+}
+
 $config = [];
 foreach ($files as $file) {
     if (stream_resolve_include_path($file)) {
