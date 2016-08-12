d691d0a390a693ceb7de3660dd0149cca89b4599
twin <twinhuang@qq.com>
2016-08-12 15:32:00 +0800

    ignore规则忽略miaoxing目录,增加travis配置 #1504

---
+++ tests/config-travis.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'user' => 'travis',
+    ],
+    'app.db' => [
+        'host' => '127.0.0.1',
+        'user' => 'travis',
+    ],
+];
