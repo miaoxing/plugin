a296abfbfe89d85edbab8aa81992eab46fb07299
twin <twinhuang@qq.com>
2016-08-13 10:03:56 +0800

    travis测试使用memcached扩展 #1504

--- tests/config-travis.php
+++ tests/config-travis.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
+    'wei' => [
+        'providers' => [
+            'cache' => 'memcached',
+        ],
+    ],
     'db' => [
         'host' => '127.0.0.1',
         'user' => 'travis',
