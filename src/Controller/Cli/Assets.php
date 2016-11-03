5469ff6ceb4a807c265c8d0d31d2506908a25a28
twinh <twinhuang@qq.com>
2016-11-03 21:52:59 +0800

    增加素材关联命令 #1828

---
+++ src/Controller/Cli/Assets.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Miaoxing\Plugin\Controller\Cli;
+
+use miaoxing\plugin\BaseController;
+use Miaoxing\Plugin\Service\Cli;
+
+/**
+ * @property Cli $cli
+ */
+class Assets extends BaseController
+{
+    public function linkAction()
+    {
+        $cli = $this->cli;
+
+        $plugins = $this->plugin->getAll();
+        foreach ($plugins as $plugin) {
+            $path = $plugin->getBasePath();
+
+            // TODO V2 只处理vendor下的插件,直到迁移完毕
+            if (strpos($path, 'vendor/') !== 0) {
+                continue;
+            }
+
+            $source = $path . '/public';
+            if (!is_dir($source)) {
+                continue;
+            }
+
+            $id = $plugin->getId();
+            $target = 'plugins/' . $id;
+            if (is_dir($target)) {
+                $this->writeln(sprintf('Remove %s', $cli->success($target)));
+                $this->remove($target);
+            }
+
+            $source = '../' . $source;
+            $this->writeln(sprintf('Symlink %s to %s', $cli->success($source), $cli->success($target)));
+            symlink($source, $target);
+        }
+    }
+
+    /**
+     * @param string $target
+     */
+    protected function remove($target)
+    {
+        if (is_link($target)) {
+            unlink($target);
+        } elseif (is_dir($target)) {
+            rmdir($target);
+        }
+    }
+
+    /**
+     * @param string $message
+     */
+    protected function writeln($message)
+    {
+        fwrite(STDERR, $message . "\n");
+    }
+}
