4ee844a59f49735e378db9f082ed93c5b919916e
twinh <twinhuang@qq.com>
2016-11-24 14:21:02 +0800

    phpcs展示检测问题 #2028

--- build/phpcs.sh
+++ build/phpcs.sh
@@ -4,7 +4,7 @@ source "${BASH_SOURCE[0]%/*}/base.sh"
 
 # 1. 执行检查
 report="reports/phpcs.txt"
-command="phpcs --standard=Miaoxing --report-file=${report} ."
+command="phpcs --standard=Miaoxing --report-file=${report} -v ."
 echo "${command}";
 
 ${command}
