1c40b5a139ae8620dc6e7c297293b7e4a763c48f
twinh <twinhuang@qq.com>
2017-02-22 15:57:17 +0800

    通过composer.json获取插件名称,解决checkout目录不是插件目录获取不到插件名称的问题 #2882

--- build/create-tests.php
+++ build/create-tests.php
@@ -4,7 +4,10 @@ require 'functions.php';
 
 $wei = init();
 
+$composerJson = json_decode(file_get_contents('composer.json'), true);
+$plugin = end(explode('/', $composerJson['name']));
+
 $wei->request->setPathInfo('/app/cli/tests/create')
-    ->set('plugin', basename(getcwd()));
+    ->set('plugin', $plugin);
 
 $wei->app();
