03137ce563700baee04839d66a6972eb95dbe855
twinh <twinhuang@qq.com>
2017-03-10 11:20:06 +0800

    尝试对php7指定5.7的phpunit #3176 #3177

--- build/install-travis.sh
+++ build/install-travis.sh
@@ -5,3 +5,7 @@ set -e
 if [ "$TRAVIS_PHP_VERSION" == "5.6" ]; then
   bash "${BASH_SOURCE[0]%/*}/install.sh"
 fi
+
+if [ "$TRAVIS_PHP_VERSION" == "7.0" ]; then
+  composer global require phpunit/phpunit:5.7
+fi
