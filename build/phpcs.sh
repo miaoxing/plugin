34958e8b5223d277a74340a6fca0466bb28101be
twinh <twinhuang@qq.com>
2017-04-25 15:04:08 +0800

    phpcs默认读取miaoxing/plugin下的配置 #3483

--- build/phpcs.sh
+++ build/phpcs.sh
@@ -7,7 +7,7 @@ standard=""
 if [ -e "phpcs.xml.dist" ]; then
   standard="phpcs.xml.dist"
 else
-  standard="Miaoxing"
+  standard="vendor/miaoxing/plugin/phpcs.xml.dist"
 fi
 
 report="reports/phpcs.txt"
