76b8c19d825f5f233ceff2cb4982411ee9673e11
twinh <twinhuang@qq.com>
2017-08-31 23:17:55 +0800

    ip限制使用ret返回

--- src/Middleware/IpRestrict.php
+++ src/Middleware/IpRestrict.php
@@ -2,23 +2,27 @@
 
 namespace Miaoxing\Plugin\Middleware;
 
+use Wei\RetTrait;
+
 class IpRestrict extends Base
 {
+    use RetTrait;
+
     protected $allowedIps;
 
     public function __invoke($next)
     {
         if (!$this->allowedIps) {
-            return $this->response
-                ->setStatusCode(403)
-                ->setContent(sprintf('Allowed IPs is not set!'));
+            $this->response->setStatusCode(403);
+
+            return $this->err('Allowed IPs is not set!');
         }
 
         $ip = $this->request->getIp();
         if (!in_array($ip, $this->allowedIps)) {
-            return $this->response
-                ->setStatusCode(403)
-                ->setContent(sprintf('IP %s is not allowed!', $ip));
+            $this->response->setStatusCode(403);
+
+            return $this->err(sprintf('IP %s is not allowed!', $ip));
         }
 
         return $next();
