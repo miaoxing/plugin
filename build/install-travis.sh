d279c8706dccfa1cbba762421fdf6e423c1e83a1
twinh <twinhuang@qq.com>
2018-01-04 00:44:49 +0800

    7.1 也安装 PHPUnit 5

--- build/install-travis.sh
+++ build/install-travis.sh
@@ -6,6 +6,6 @@ if [ "$TRAVIS_PHP_VERSION" == "5.6" ]; then
   bash "${BASH_SOURCE[0]%/*}/install.sh"
 fi
 
-if [ "$TRAVIS_PHP_VERSION" == "7.0" ]; then
+if [ "$TRAVIS_PHP_VERSION" == "7.0" ] || [ "$TRAVIS_PHP_VERSION" == "7.1" ]; then
   composer global require phpunit/phpunit:5.7
 fi
