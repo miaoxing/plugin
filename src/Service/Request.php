b51f9253c8ebd68650616955b787756f686559d7
twinh <twinhuang@qq.com>
2018-01-23 10:19:14 +0800

    通过extraKeys的计算,解决 if ($req['xxx']) 会尝试 $req['xxx'] = null 的问题

--- src/Service/Request.php
+++ src/Service/Request.php
@@ -4,6 +4,13 @@ namespace Miaoxing\Plugin\Service;
 
 class Request extends \Wei\Request
 {
+    /**
+     * 因为 &offsetGet 产生的额外键名
+     *
+     * @var array
+     */
+    protected $extraKeys = [];
+
     public function json()
     {
         return $this->acceptJson();
@@ -12,5 +19,58 @@ class Request extends \Wei\Request
     public function csv()
     {
         return $this['_format'] == 'csv';
+    }
+
+    public function &offsetGet($offset)
+    {
+        if (!isset($this->data[$offset])) {
+            $this->extraKeys[$offset] = true;
+        }
+
+        return $this->data[$offset];
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        unset($this->extraKeys[$offset]);
+
+        return $this->data[$offset] = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->extraKeys[$offset]);
+        unset($this->data[$offset]);
+    }
+
+    public function toArray()
+    {
+        $this->removeExtraKeys();
+
+        return parent::toArray();
+    }
+
+    public function getIterator()
+    {
+        $this->removeExtraKeys();
+
+        return new \ArrayIterator($this->data);
+    }
+
+    protected function removeExtraKeys()
+    {
+        foreach ($this->extraKeys as $offset => $value) {
+            if ($this->data[$offset] === null) {
+                unset($this->data[$offset]);
+            }
+        }
+        $this->extraKeys = [];
+    }
+
+    public function count()
+    {
+        $this->removeExtraKeys();
+
+        return parent::count();
     }
 }
