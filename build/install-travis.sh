dd5515ba88f8d2e615b9958f549130cb5b7ba494
twinh <twinhuang@qq.com>
2018-01-31 20:57:34 +0800

    htmllint-cli 移到依赖中, yarn 改为平台自己安装

--- build/install-travis.sh
+++ build/install-travis.sh
@@ -3,6 +3,13 @@
 set -e
 
 if [ "$TRAVIS_PHP_VERSION" == "5.6" ]; then
+  # Install yarn
+  curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.3.2
+  export PATH=$HOME/.yarn/bin:$PATH
+
+  # Install dependencies
+  yarn
+
   bash "${BASH_SOURCE[0]%/*}/install.sh"
 fi
 
--- build/install.sh
+++ build/install.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Install yarn
-curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.3.2
-export PATH=$HOME/.yarn/bin:$PATH
-
 # Set Miaoxing coding standards
 vendor/bin/phpcs --config-set installed_paths vendor/miaoxing/coding-standards
 vendor/bin/phpcs -i
 
 PATH=~/.composer/vendor/bin:$PATH
-
-yarn
-
-yarn add htmllint-cli@0.0.5 -g
