8aad04e9e981b84956d6864de0802cad57e5bc94
twinh <twinhuang@qq.com>
2018-01-31 20:30:32 +0800

    移除暂时不需要的PHP-CS-Fixer,更正yarn命令

--- build/install.sh
+++ build/install.sh
@@ -4,11 +4,6 @@
 curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.3.2
 export PATH=$HOME/.yarn/bin:$PATH
 
-# Install PHP-CS-Fixer
-curl -L https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v1.12.1/php-cs-fixer.phar -o php-cs-fixer \
-    && chmod a+x php-cs-fixer \
-    && mv php-cs-fixer ~/.composer/vendor/bin/php-cs-fixer
-
 # Set Miaoxing coding standards
 vendor/bin/phpcs --config-set installed_paths vendor/miaoxing/coding-standards
 vendor/bin/phpcs -i
@@ -17,4 +12,4 @@ PATH=~/.composer/vendor/bin:$PATH
 
 yarn
 
-yarn add -g htmllint-cli@0.0.5
+yarn add htmllint-cli@0.0.5 -g
