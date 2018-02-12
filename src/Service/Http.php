120fd7856d300a7c8b6e605b6473487c6a24c7fb
twinh <twinhuang@qq.com>
2018-02-12 10:30:10 +0800

    简化返回值

--- src/Service/Http.php
+++ src/Service/Http.php
@@ -17,7 +17,7 @@ class Http extends \Wei\Http
     public function toRet(array $data = [])
     {
         if ($this->isSuccess()) {
-            return $this->suc($data + ['data' => $this->getResponse()]);
+            return $this->suc($data + $this->getResponse());
         }
 
         $e = $this->getErrorException();
