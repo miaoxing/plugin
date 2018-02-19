6cfc3b115dea08045585a169df54d919e23a36a6
twinh <twinhuang@qq.com>
2018-02-19 21:47:35 +0800

    默认js,css移除swipe

--- resources/views/layouts/jqm.php
+++ resources/views/layouts/jqm.php
@@ -6,17 +6,13 @@
   <meta name="format-detection" content="telephone=no">
   <title><?= $e($wei->page->getTitle()) ?></title>
   <link rel="stylesheet" href="//cdn.bootcss.com/jquery-mobile/1.4.0/jquery.mobile.min.css">
-  <link rel="stylesheet" href="<?= $asset([
-    'plugins/app/css/swipe.css',
-    'plugins/app/css/jqm.css',
-  ]) ?>">
+  <link rel="stylesheet" href="<?= $asset('plugins/app/css/jqm.css') ?>">
   <?= $wei->page->renderHead() ?>
   <script src="//cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
   <script src="//cdn.bootcss.com/jquery-mobile/1.4.0/jquery.mobile.min.js"></script>
   <script src="<?= $asset([
     'comps/requirejs/require.min.js',
     'plugins/app/js/require-config.js',
-    'comps/Swipe/swipe.js',
     'plugins/app/js/app.js',
     'assets/popup.js',
     'assets/jqueryMobile.js',
