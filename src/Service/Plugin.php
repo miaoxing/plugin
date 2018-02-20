27e0b3b6b54914a3c303ba8002618924d67bf28a
twinh <twinhuang@qq.com>
2018-02-20 10:39:01 +0800

    更正dash方法错误

--- src/Service/Plugin.php
+++ src/Service/Plugin.php
@@ -718,7 +718,7 @@ class Plugin extends BaseService
      */
     protected function dash($name)
     {
-        return strtolower(preg_replace('/([A-Z])/', '-$1', $name));
+        return strtolower(preg_replace('~(?<=\\w)([A-Z])~', '-$1', $name));
     }
 
     /**
