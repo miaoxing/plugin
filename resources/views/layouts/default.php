2753380f8ccf147613ea64e748629feaa25e2967
twinh <twinhuang@qq.com>
2018-11-12 17:46:21 +0800

    feat: 前端资源改为webpack加载

--- resources/views/layouts/default.php
+++ resources/views/layouts/default.php
@@ -1,3 +1,7 @@
+<?php
+
+wei()->wpAsset->addRevFile('dist2/app-v1-assets-hash.json');
+?>
 <!DOCTYPE html>
 <html<?= $wei->ua->isIOS() ? ' class="ios"' : '' ?>>
 <head>
@@ -5,12 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
   <meta name="format-detection" content="telephone=no">
   <title><?= $e($wei->page->getTitle()) ?></title>
-  <link rel="stylesheet" href="<?= $asset([
-    'comps/bootstrap-custom/css/bootstrap.min.css',
-    'comps/bootstrap-mobile/dist/css/bootstrap-mobile.css',
-    'plugins/app/css/tips.css',
-    'plugins/app/css/app.css',
-  ]) ?>">
+  <link rel="stylesheet" href="<?= $this->wpAsset('app-v1.css') ?>"/>
   <?php require $view->getFile('@plugin/layouts/default-css.php') ?>
   <?= $wei->page->renderHead() ?>
 </head>
@@ -21,19 +20,7 @@
 <div id="root"></div>
 <?= $block->get('html') ?>
 
-<script src="<?= $mainJs = $asset([
-  'comps/requirejs/require.min.js',
-  'comps/jquery/jquery.min.js',
-  'comps/jquery-list/jquery-list.js',
-  'comps/bootstrap-custom/js/bootstrap.min.js',
-  'comps/bootstrap-mobile/dist/js/bootstrap-mobile.min.js',
-  'plugins/app/js/app.js',
-  'plugins/app/js/require-config.js',
-  'plugins/app/js/tips.js',
-  'plugins/app/js/bootstrap-popup.js',
-  'plugins/app/js/bootstrap-ajax-tips.js',
-]) ?>"></script>
-<script>window.requirejs || document.write('<script src="<?= $asset->fallback($mainJs) ?>"><\/script>')</script>
+<script src="<?= $this->wpAsset('app-v1.js') ?>"></script>
 <script>
   $.extend($, <?= json_encode($app->getConfig()) ?>);
   var wei = <?= json_encode($js) ?>;
