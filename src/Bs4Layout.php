aaac1da4f35797d57743e18d558039397dcd6b5e
twinh <twinhuang@qq.com>
2018-12-02 21:41:55 +0800

    feat: 合并bs4layout

--- src/Bs4Layout.php
+++ src/Bs4Layout.php
@@ -2,12 +2,24 @@
 
 namespace Miaoxing\Plugin;
 
+use Miaoxing\Plugin\Service\App;
+use Miaoxing\Plugin\Service\View;
+
+/**
+ * @property View $view
+ * @property App $app
+ */
 trait Bs4Layout
 {
     public function __construct(array $options = [])
     {
         parent::__construct($options);
 
-        $this->view->setDefaultLayout('@plugin/layouts/default-bs4.php');
+        if ($this->app->isAdmin()) {
+            $this->view->setDefaultLayout('@admin/admin/layout-bs4.php');
+        } else {
+            $this->view->setDefaultLayout('@plugin/layouts/default-bs4.php');
+        }
+        $this->app->setDefaultViewFile('@app/_default-bs4.php');
     }
 }
