ddb1f3fb9f015112a6772728149bbcbab823da4e
twinh <twinhuang@qq.com>
2018-12-02 22:27:59 +0800

    feat: 更新前台页面为纯bs4

--- resources/views/layouts/default-bs4.php
+++ resources/views/layouts/default-bs4.php
@@ -1,27 +1,18 @@
-<?php
-
-wei()->wpAsset->addRevFile('dist2/app-bs4-assets-hash.json');
-?>
 <!DOCTYPE html>
-<html>
+<html lang="zh-CN">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
   <meta name="format-detection" content="telephone=no">
   <title><?= $e($wei->page->getTitle()) ?></title>
-  <link rel="stylesheet" href="<?= $this->wpAsset('app-bs4.css') ?>"/>
   <?= $wei->page->renderHead() ?>
 </head>
 <body>
 <?php $event->trigger('bodyStart') ?>
-
 <?= $content ?>
 <div id="root"></div>
-<?= $block->get('html') ?>
-
-<script src="<?= $this->wpAsset('app-bs4.js') ?>"></script>
 <script>
-  $.extend($, <?= json_encode($app->getConfig()) ?>);
+  var wei = <?= json_encode($js) ?>;
 </script>
 <?= $wei->page->renderFooter() ?>
 </body>
