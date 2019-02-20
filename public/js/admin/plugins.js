1d742d66a59a0c335fcfedb8158145bd01904803
twinh <twinhuang@qq.com>
2019-02-20 17:31:53 +0800

    refactoring: deparam => unparam

--- public/js/admin/plugins.js
+++ public/js/admin/plugins.js
@@ -1,4 +1,4 @@
-define(['template', 'plugins/admin/js/data-table', 'form', 'jquery-deparam'], function (template) {
+define(['template', 'plugins/admin/js/data-table', 'form', 'jquery-unparam'], function (template) {
   var Plugins = function () {
     // do nothing.
   };
