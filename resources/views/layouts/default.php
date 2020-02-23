f5904427ed7b9fdedd74674971d5c5a5a5804ed2
twinh <twinhuang@qq.com>
2020-02-23 10:17:27 +0800

    refactor: 移除启动非必须的功能

--- resources/views/layouts/default.php
+++ resources/views/layouts/default.php
@@ -10,7 +10,6 @@ $wei->page->addPluginAssets('app');
   <meta name="format-detection" content="telephone=no">
   <title><?= $e($wei->page->getTitle()) ?></title>
   <?= $wei->page->renderHead() ?>
-  <?php require $view->getFile('@plugin/layouts/default-css.php') ?>
 </head>
 <body>
 <?php $event->trigger('bodyStart') ?>
@@ -23,18 +22,5 @@ $wei->page->addPluginAssets('app');
   var wei = <?= json_encode($js) ?>;
 </script>
 <?= $wei->page->renderFooter() ?>
-<script>
-  if (/iphone|ipod|ipad/i.test(navigator.appVersion)) {
-    var event = function (e) {
-      ['input', 'textarea', 'select'].includes(e.target.localName) && document.body.scrollIntoView(false)
-    };
-    $(document).on('show.bs.modal', function () {
-      document.addEventListener('blur', event, true);
-    });
-    $(document).on('hide.bs.modal', function () {
-      document.removeEventListener('blur', event, true);
-    });
-  }
-</script>
 </body>
 </html>
