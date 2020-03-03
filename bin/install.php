91febd1bccbf08fa4bdcf2c24e6c564d87c76b35
twinh <twinhuang@qq.com>
2020-03-03 21:58:56 +0800

    chore: 更新测试安装逻辑

--- bin/install.php
+++ bin/install.php
@@ -19,35 +19,10 @@ $db = $wei->db;
 $db->executeUpdate('CREATE DATABASE IF NOT EXISTS app;');
 $db->useDb('app');
 
-// TODO 待更新为migration模式?
-// 3. 获取各插件的SQL文件
-$sqlFiles = [];
-foreach ($wei->plugin->getAll() as $plugin) {
-    $basePath = $plugin->getBasePath();
-    $sqlFiles = array_merge($sqlFiles, glob(($basePath ?: '.') . '/docs/*.sql'));
-}
+// 3. 执行迁移语句
+$wei->migration->migrate();
 
-// 4. 逐个运行
-// 临时指定所在数据库
-$appTables = [
-    'apps',
-];
-foreach ($sqlFiles as $file) {
-    $table = basename($file, '.sql');
-    $db = in_array($table, $appTables) ? $wei->appDb : $wei->db;
-    $result = $db->fetch('SHOW TABLES LIKE ?', $table);
-    if (!$result) {
-        $db->executeUpdate(file_get_contents($file));
-    }
-}
-
-// 5. 逐个安装插件
+// 4. 逐个安装插件
 foreach ($wei->plugin->getAll() as $plugin) {
     $wei->plugin->install($plugin->getId());
 }
-
-// 6. 执行迁移语句
-$wei->migration->migrate();
-
-// 7. 尝试移除缓存
-$wei->cache->remove('tableFieldsapp.apps');
