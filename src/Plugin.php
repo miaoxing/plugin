133ddb8f266a29d609ef080bc6251e24f2dc891c
twinh <twinhuang@qq.com>
2020-03-04 15:49:33 +0800

    chore: 移除无用函数

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,14 +2,9 @@
 
 namespace Miaoxing\Plugin;
 
-class Plugin extends \Miaoxing\Plugin\BasePlugin
+class Plugin extends BasePlugin
 {
     protected $name = '插件核心';
 
-    protected $description = '插件管理,包括插件获取,安装,卸载等功能';
-
-    public function controllerInit()
-    {
-        // 服务需要提供给前端配置，如何处理？
-    }
+    protected $description = '插件管理,包括插件安装,卸载等功能';
 }
