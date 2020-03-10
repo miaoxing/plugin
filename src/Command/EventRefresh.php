6e431c4efd71db7a4e2c0d0ac1e211d6f583df65
twinh <twinhuang@qq.com>
2020-03-10 17:13:53 +0800

    feat: 增加 event:refresh 来刷新应用的事件缓存

---
+++ src/Command/EventRefresh.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Miaoxing\Plugin\Command;
+
+use Miaoxing\Services\Command\BaseCommand;
+
+class EventRefresh extends BaseCommand
+{
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'event:cache {app=app : The name of the app}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the event cache';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        wei()->app->setNamespace($this->argument('app'));
+        wei()->plugin->getEvents(true);
+        $this->info('Refreshed the event cache!');
+    }
+}
