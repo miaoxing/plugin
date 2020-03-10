442be2f83f7ad722330c1f5b231e07c1f6c50714
twinh <twinhuang@qq.com>
2020-03-10 17:23:45 +0800

    refactor: 统一使用一个布局

--- resources/views/layouts/default.php
+++ resources/views/layouts/default.php
@@ -2,6 +2,8 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="renderer" content="webkit">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
   <meta name="format-detection" content="telephone=no">
   <title><?= $e($wei->page->getTitle()) ?></title>
@@ -10,7 +12,7 @@
 <body>
 <?php $event->trigger('bodyStart') ?>
 
-<?= $content ?? '<div id="root"></div>' ?>
+<?= $content ?: '<div id="root"></div>' ?>
 <?= $block->get('html') ?>
 
 <?= $wei->page->renderFooter() ?>
