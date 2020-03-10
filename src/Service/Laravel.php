b1aa2b360b806536d583c217c40d141416b428a2
twinh <twinhuang@qq.com>
2020-03-10 11:17:30 +0800

    refactor: 加入自定义的 http kernel，进一步简化 laravel 启动

--- src/Service/Laravel.php
+++ src/Service/Laravel.php
@@ -12,6 +12,7 @@ use Illuminate\Redis\RedisManager;
 use Miaoxing\Plugin\BaseService;
 use Illuminate\Support\Facades\Facade;
 use Miaoxing\Plugin\Laravel\ConsoleKernel;
+use Miaoxing\Plugin\Laravel\HttpKernel;
 use Miaoxing\Queue\Service\Queue;
 use Miaoxing\Services\Service\StaticTrait;
 use Wei\Db;
@@ -50,57 +51,10 @@ class Laravel extends BaseService
 
     protected function createApp()
     {
-        /*
-        |--------------------------------------------------------------------------
-        | Create The Application
-        |--------------------------------------------------------------------------
-        |
-        | The first thing we will do is create a new Laravel application instance
-        | which serves as the "glue" for all the components of Laravel, and is
-        | the IoC container for the system binding all of the various parts.
-        |
-        */
+        $app = new \Illuminate\Foundation\Application($_ENV['APP_BASE_PATH'] ?? realpath('.'));
 
-        $app = new \Illuminate\Foundation\Application(
-            $_ENV['APP_BASE_PATH'] ?? realpath('.')
-        );
-
-        /*
-        |--------------------------------------------------------------------------
-        | Bind Important Interfaces
-        |--------------------------------------------------------------------------
-        |
-        | Next, we need to bind some important interfaces into the container so
-        | we will be able to resolve them when needed. The kernels serve the
-        | incoming requests to this application from both the web and CLI.
-        |
-        */
-
-        $app->singleton(
-            \Illuminate\Contracts\Http\Kernel::class,
-            \App\Http\Kernel::class
-        );
-
-        $app->singleton(
-            \Illuminate\Contracts\Console\Kernel::class,
-            ConsoleKernel::class
-        );
-
-        $app->singleton(
-            \Illuminate\Contracts\Debug\ExceptionHandler::class,
-            \App\Exceptions\Handler::class
-        );
-
-        /*
-        |--------------------------------------------------------------------------
-        | Return The Application
-        |--------------------------------------------------------------------------
-        |
-        | This script returns the application instance. The instance is given to
-        | the calling script so we can separate the building of the instances
-        | from the actual running of the application and sending responses.
-        |
-        */
+        $app->singleton(\Illuminate\Contracts\Http\Kernel::class, HttpKernel::class);
+        $app->singleton(\Illuminate\Contracts\Console\Kernel::class, ConsoleKernel::class);
 
         return $app;
     }
