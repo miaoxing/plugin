1795c28d6525aa95bbcf8417608139f626fc678c
twinh <twinhuang@qq.com>
2020-03-12 21:40:19 +0800

    refactor: 迁移创建服务脚本

---
+++ resources/stubs/service.php
@@ -0,0 +1,13 @@
+<?= '<?php', "\n" ?>
+
+namespace <?= $namespace ?>;
+
+use Miaoxing\Plugin\BaseService;
+
+/**
+ * <?= $class, "\n" ?>
+ */
+class <?= $class ?> extends BaseService
+{
+
+}
---
+++ src/Command/GService.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Miaoxing\Plugin\Command;
+
+use Miaoxing\Plugin\BasePlugin;
+use Symfony\Component\Console\Input\InputArgument;
+
+class GService extends BaseCommand
+{
+    protected static $defaultName = 'g:service';
+
+    protected function configure()
+    {
+        $this
+            ->addArgument('name', InputArgument::REQUIRED, 'The name of service')
+            ->addArgument('plugin-id', InputArgument::REQUIRED, 'The id of plugin');
+    }
+
+    /**
+     * @return int
+     * @throws \Exception
+     */
+    protected function handle()
+    {
+        $name = $this->input->getArgument('name');
+        $id = $this->input->getArgument('plugin-id');
+
+        $plugin = wei()->plugin->getOneById($id);
+
+        $class = ucfirst($name);
+        $file = $this->getFile($plugin, $class);
+        $this->createDir(dirname($file));
+
+        $namespace = $this->getNamespace($plugin);
+
+        $this->createFile($file, $namespace, $class);
+
+        $this->plugin->getConfig(true);
+
+        // TODO
+        //$this->app->dispatch('cli/docs', 'create');
+
+        return $this->suc('创建成功');
+    }
+
+    protected function getFile(BasePlugin $plugin, $name)
+    {
+        return $plugin->getBasePath() . '/src/Service/' . ucfirst($name) . '.php';
+    }
+
+    protected function createDir($dir)
+    {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+            chmod($dir, 0777);
+        }
+    }
+
+    protected function getNamespace(BasePlugin $plugin)
+    {
+        $class = get_class($plugin);
+        $parts = explode('\\', $class);
+        array_pop($parts);
+
+        return implode('\\', $parts) . '\Service';
+    }
+
+    protected function createFile($file, $namespace, $class)
+    {
+        $this->suc('生成文件 ' . $file);
+
+        ob_start();
+        require $this->plugin->getById('plugin')->getBasePath() .'/resources/stubs/service.php';
+        $content = ob_get_clean();
+
+        file_put_contents($file, $content);
+        chmod($file, 0777);
+    }
+}
