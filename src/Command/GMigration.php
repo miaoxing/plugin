2cc02e297901a1b8128609f56311c48929d1b3a6
twinh <twinhuang@qq.com>
2020-03-13 16:52:22 +0800

    feat: 增加 g:migration 命令，解耦 migration 和 plugin

---
+++ src/Command/GMigration.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Miaoxing\Plugin\Command;
+
+use Miaoxing\Services\Service\Migration;
+use ReflectionClass;
+use Symfony\Component\Console\Input\InputArgument;
+
+class GMigration extends BaseCommand
+{
+    protected function configure()
+    {
+        $this->setDescription('Generate a plugin migration class')
+            ->addArgument('name', InputArgument::REQUIRED, 'The name of the migration')
+            ->addArgument('plugin', InputArgument::OPTIONAL, 'The name of the plugin');
+    }
+
+    /**
+     * @return int|void
+     * @throws \ReflectionException
+     * @throws \Exception
+     */
+    protected function handle()
+    {
+        $plugin = wei()->plugin->getOneById($this->getArgument('plugin'));
+        $path = $plugin->getBasePath() . '/src/Migration';
+        $reflection = new ReflectionClass($plugin);
+        $namespace = $reflection->getNamespaceName() . '\\Migration';
+
+        Migration::setOutput($this->output)->create([
+            'name' => $this->getArgument('name'),
+            'path' => $path,
+            'namespace' => $namespace,
+        ]);
+    }
+}
