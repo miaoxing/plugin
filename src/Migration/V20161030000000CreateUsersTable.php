214a30d955c055af83afabe8859e7ff1aba93912
twinh <twinhuang@qq.com>
2020-04-09 18:12:28 +0800

    refactor: 非核心字段移到 user 插件中

--- src/Migration/V20161030000000CreateUsersTable.php
+++ src/Migration/V20161030000000CreateUsersTable.php
@@ -17,20 +17,18 @@ class V20161030000000CreateUsersTable extends BaseMigration
             ->id()
             ->int('app_id')
             ->string('out_id', 32)
-            ->char('wechat_open_id', 28)->comment('微信的OpenID')
-            ->char('wechat_union_id', 29)
             ->bool('admin')
             ->string('nick_name', 32)
             ->string('remark_name', 32)
             ->string('username', 64)
             ->string('name', 16)
-            ->string('email', 256)
+            ->string('email')
             ->string('mobile', 16)
             ->timestamp('mobile_verified_at')->comment('手机校验时间')
             ->string('phone', 16)
             ->string('salt', 32)
             ->string('password', 128)
-            ->tinyInt('sex')
+            ->tinyInt('sex')->defaults(1)
             ->string('country', 32)
             ->string('province', 32)
             ->string('city', 32)
@@ -38,21 +36,11 @@ class V20161030000000CreateUsersTable extends BaseMigration
             ->string('address', 128)
             ->string('signature', 64);
 
-        $table->string('avatar', 256)
-            ->int('group_id')->comment('用户组')
-            ->decimal('money', 16, 2)->comment('账户余额')
-            ->decimal('recharge_money', 16, 2)->comment('充值账户余额')
-            ->int('score')->comment('积分')
+        $table->bool('enable')->defaults(1)->comment('是否启用')
+            ->string('avatar')
             ->timestamp('last_login_at')
-            ->timestamp('unsubscribed_at')->comment('取关时间')
-            ->bool('is_subscribed')
-            ->bool('enable')->defaults(1)->comment('是否启用')
-            ->string('source', 16)->comment('用户来源')
             ->timestamps()
-            ->userstamps();
-
-        $table->index('wechat_open_id')
-            ->index('is_subscribed')
+            ->userstamps()
             ->exec();
 
         $salt = wei()->password->generateSalt();
