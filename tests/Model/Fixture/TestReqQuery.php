c60f8a84d2b88036614602cda3c3c710f76a9c2f
twinh <twinhuang@qq.com>
2020-05-12 20:54:59 +0800

    test: 解决 req 测试失败

--- tests/Model/Fixture/TestReqQuery.php
+++ tests/Model/Fixture/TestReqQuery.php
@@ -2,10 +2,13 @@
 
 namespace MiaoxingTest\Plugin\Model\Fixture;
 
+use Miaoxing\Plugin\Model\ReqQueryTrait;
 use Miaoxing\Plugin\Service\Model;
 
 class TestReqQuery extends Model
 {
+    use ReqQueryTrait;
+
     public function detail()
     {
         return $this->hasOne(TestReqQueryDetail::class);
