4d528d711549193b87cfce0e5098290f5ffa1e8a
twinh <twinhuang@qq.com>
2020-05-13 01:00:56 +0800

    test: 进一步简化配置

--- tests/config.php
+++ tests/config.php
@@ -3,13 +3,12 @@
 use Miaoxing\Plugin\Service\Model;
 
 return [
-    // 服务容器
+    /* @see \Wei\Wei 服务容器 */
     'wei' => [
         'debug' => true,
         'inis' => [
             'display_errors' => true,
             'error_reporting' => E_ALL,
-            'date.timezone' => 'Asia/Shanghai',
         ],
         'aliases' => [
             'plugin' => \Miaoxing\Plugin\Service\Plugin::class,
@@ -19,15 +18,15 @@ return [
             'cache' => 'arrayCache',
         ],
         'preload' => [
-            'error',
             'plugin',
         ],
     ],
+    /* @see \Wei\Db 数据库 */
     'db' => [
         'host' => 'mysql',
         'user' => 'root',
         'dbname' => 'miaoxing',
-        'password' => getenv('MYSQL_PASSWORD') ?: 'password',
+        'password' => 'password',
         'recordClass' => Model::class,
     ],
     'error.logger' => [
