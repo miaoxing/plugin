9d860130c3af334eb2a6db0e3130ef7955a838db
twinh <twinhuang@qq.com>
2020-05-13 00:46:54 +0800

    test: 移除非必须的测试配置

--- tests/config.php
+++ tests/config.php
@@ -13,7 +13,6 @@ return [
         ],
         'aliases' => [
             'plugin' => \Miaoxing\Plugin\Service\Plugin::class,
-            'request' => \Miaoxing\Services\Service\Request::class,
             'classMap' => \Miaoxing\Services\Service\ClassMap::class,
         ],
         'providers' => [
@@ -24,55 +23,14 @@ return [
             'plugin',
         ],
     ],
-    'app' => [
-        'namespace' => 'app',
-    ],
     'db' => [
         'host' => 'mysql',
-        'port' => 3306,
         'user' => 'root',
         'dbname' => 'miaoxing',
-        'charset' => 'utf8mb4',
         'password' => getenv('MYSQL_PASSWORD') ?: 'password',
         'recordClass' => Model::class,
     ],
-    'router' => [
-        'namespaces' => ['admin', 'api', 'cli'],
-    ],
-    // 视图
-    'view' => [
-        'defaultLayout' => '@plugin/layouts/default.php',
-    ],
-    // 语言翻译
-    't' => [
-        'locale' => 'zh-CN',
-    ],
-    'phpFileCache' => [
-        'dir' => 'data/cache',
-    ],
-    'nearCache' => [
-        'providers' => [
-            'front' => 'arrayCache',
-            'back' => 'cache',
-        ],
-    ],
-    'logger' => [
-        'dir' => 'data/logs',
-    ],
     'error.logger' => [
-        'dir' => 'data/logs',
-    ],
-    'mail' => [
-        'options' => [
-            'Mailer' => 'smtp',
-        ],
-    ],
-    'schema' => [
-        'charset' => 'utf8mb4',
-        'collate' => 'utf8mb4_unicode_ci',
-    ],
-    'config' => [
-        // TODO 测试时不用写入配置
-        'version' => '2200-01-01',
+
     ],
 ];
