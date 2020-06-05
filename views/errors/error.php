e8083277db3d0bef520633e0795e9ed7cb0d4469
twinh <twinhuang@qq.com>
2020-06-05 18:50:56 +0800

    chore: 简化错误

--- views/errors/error.php
+++ views/errors/error.php
@@ -3,10 +3,10 @@
 <head>
   <meta name="viewport" content="width=device-width">
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>请求错误</title>
+  <title><?= $title ?></title>
 </head>
 <body>
-  <h1>请求错误</h1>
-  <p>很抱歉，您的请求数据错误，请检查后再试。</p>
+  <h1><?= $message ?></h1>
+  <p>很抱歉，请求失败，请稍后再试。</p>
 </body>
 </html>
