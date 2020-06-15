57a3c2a37af44c98f7e4336e3bc37ae8e7445f52
twinh <twinhuang@qq.com>
2020-06-15 15:30:38 +0800

    test: str, const 测试移到 plugin 中 2

---
+++ tests/Fixture/SexConst.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MiaoxingTest\Plugin\Fixture;
+
+use Miaoxing\Plugin\ConstTrait;
+
+/**
+ * 性别常量服务
+ */
+class SexConst
+{
+    use ConstTrait;
+
+    const SEX_MALE = 1;
+
+    const SEX_FEMALE = 2;
+
+    protected $sexNames = [
+        self::SEX_MALE => '男',
+        self::SEX_FEMALE => '女',
+    ];
+}
