b637d563795e96423e50829afff23ad495313e23
twinh <twinhuang@qq.com>
2020-09-27 16:00:52 +0800

    test: 调用 getData 不会导致 Session 中出现 null 的 user 值

---
+++ tests/Auth/SessionAuthTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MiaoxingTest\Plugin\Auth;
+
+use Miaoxing\Plugin\Auth\SessionAuth;
+use Miaoxing\Plugin\Test\BaseTestCase;
+
+/**
+ * @mixin \SessionMixin
+ */
+class SessionAuthTest extends BaseTestCase
+{
+    public function testGetDataWontCauseUserKeyToNull()
+    {
+        $auth = new SessionAuth();
+        $data = $auth->getData();
+        $this->assertSame([], $data);
+        $this->assertArrayNotHasKey('user', $this->session);
+    }
+}
