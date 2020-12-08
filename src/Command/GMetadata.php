31f4e13231d82c94d0e74f04874128f36251931b
twinh <twinhuang@qq.com>
2020-12-08 15:21:01 +0800

    fix(GMetadata): 解决获取 trait 时缺少依赖的 trait 的问题

--- src/Command/GMetadata.php
+++ src/Command/GMetadata.php
@@ -43,17 +43,28 @@ final class GMetadata extends BaseCommand
     }
 
     /**
-     * @param string $class
+     * @param object $class
      * @param bool $autoload
      * @return array
-     * @see http://php.net/manual/en/function.class-uses.php#110752
+     * @see https://www.php.net/manual/en/function.class-uses.php#112671
      */
     public function classUsesDeep($class, $autoload = true)
     {
         $traits = [];
+
+        // Get traits of all parent classes
         do {
             $traits = array_merge(class_uses($class, $autoload), $traits);
         } while ($class = get_parent_class($class));
+
+        // Get traits of all parent traits
+        $traitsToSearch = $traits;
+        while (!empty($traitsToSearch)) {
+            $newTraits = class_uses(array_pop($traitsToSearch), $autoload);
+            $traits = array_merge($newTraits, $traits);
+            $traitsToSearch = array_merge($newTraits, $traitsToSearch);
+        };
+
         foreach ($traits as $trait => $same) {
             $traits = array_merge(class_uses($trait, $autoload), $traits);
         }
