4bcd7b64365b1d84b5c29b202787cd09ecada6ca
twinh <twinhuang@qq.com>
2021-06-13 11:47:33 +0800

    feat(Resource): 增加 `Resource` 功能，用于将模型对象转换为接口响应数组，参考自 Laravel

---
+++ src/Resource/MergeValue.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Miaoxing\Plugin\Resource;
+
+class MergeValue
+{
+    protected $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
---
+++ src/Resource/MissingValue.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Miaoxing\Plugin\Resource;
+
+class MissingValue
+{
+}
