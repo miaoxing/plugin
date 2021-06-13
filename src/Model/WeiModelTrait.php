b352c14337fc4875c654bfd44144fd79d6fd0f00
twinh <twinhuang@qq.com>
2021-06-13 16:22:31 +0800

    feat(Model): `toRet` 支持传入 Resource 类名和对象
    BREAKING CHANGE: `toRet` 方法的参数去掉 `array` 类型

--- src/Model/WeiModelTrait.php
+++ src/Model/WeiModelTrait.php
@@ -3,6 +3,7 @@
 namespace Miaoxing\Plugin\Model;
 
 use InvalidArgumentException;
+use Miaoxing\Plugin\Resource\BaseResource;
 use Miaoxing\Plugin\Service\Cls;
 use Miaoxing\Plugin\Service\Str;
 use Miaoxing\Plugin\Service\User;
@@ -540,16 +541,29 @@ trait WeiModelTrait
     /**
      * Returns the success result with model data
      *
-     * @param array $merge
+     * @param array|string|BaseResource|mixed $merge
      * @return Ret
      * @svc
      */
-    protected function toRet(array $merge = []): Ret
+    protected function toRet($merge = []): Ret
     {
-        if ($this->coll) {
-            return $this->collToRet($merge);
+        if (is_array($merge)) {
+            $data = $merge;
+        } elseif ($merge instanceof BaseResource) {
+            $data = $merge->toArray($this);
+        } elseif (is_subclass_of($merge, BaseResource::class)) {
+            $data = $merge::toArray($this);
         } else {
-            return $this->suc($merge + ['data' => $this])->setMetadata('model', $this);
+            throw new \InvalidArgumentException(sprintf(
+                'Expected argument of type array, instance of BaseResource, or subclass of BaseReource, "%s" given',
+                is_object($merge) ? get_class($merge) : gettype($merge)
+            ));
+        }
+
+        if ($this->coll) {
+            return $this->collToRet($data);
+        } else {
+            return $this->suc($data + ['data' => $this])->setMetadata('model', $this);
         }
     }
 
