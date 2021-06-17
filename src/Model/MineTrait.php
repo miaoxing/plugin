2786671fc08406c5a7b8a121f7bd296892043937
twinh <twinhuang@qq.com>
2021-06-17 18:53:54 +0800

    feat(MineTrait): 增加 `MineTrait`，用于查询当前用户的记录

--- src/Model/MineTrait.php
+++ src/Model/MineTrait.php
@@ -4,35 +4,58 @@ namespace Miaoxing\Plugin\Model;
 
 use Miaoxing\Plugin\Service\User;
 
+/**
+ * Add functions to the model to query the current logged in user's record
+ *
+ *  @property-read string $userIdColumn The column contains user id
+ */
 trait MineTrait
 {
-    protected $userIdColumn = 'userId';
-
+    /**
+     * @var self|null
+     */
     protected static $mineCache;
 
     /**
-     * QueryBuilder: 筛选属于当前登录用户的记录
+     * Query: Filter by current user id
      *
      * @return $this
+     * @svc
      */
-    public function mine()
+    protected function mine(): self
     {
-        return $this->andWhere(['userId' => User::id()]);
+        return $this->where([$this->getUserIdColumn() => (int) User::id()]);
     }
 
     /**
+     * Query: find or init the record by current user id
+     *
      * @return $this
+     * @svc
      */
-    public function findOrInitMine()
+    protected function findOrInitMine(): self
     {
-        return $this->mine()->findOrInit();
+        return $this->findOrInitBy([$this->getUserIdColumn() => (int) User::id()]);
     }
 
     /**
-     * @return $this
+     * Query: find or init and cache the record by current user id
+     *
+     * @return self
+     * @svc
      */
-    public function findOrInitMineCached()
+    protected function findOrInitMineCached(): self
     {
-        return static::$mineCache ? static::$mineCache : static::$mineCache = $this->findOrInitMine();
+        return static::$mineCache ?: static::$mineCache = $this->findOrInitMine();
+    }
+
+    /**
+     * Returns the user id column
+     *
+     * @return string
+     */
+    protected function getUserIdColumn(): string
+    {
+        return $this->userIdColumn ?? $this->convertToPhpKey('user_id');
     }
 }
