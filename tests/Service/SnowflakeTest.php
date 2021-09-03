a2e79cc14e729f54e663da103d638a174a8733e3
twinh <twinhuang@qq.com>
2021-09-03 16:19:33 +0800

    test(plugin): 测试 `Snowflake::parse`

--- tests/Service/SnowflakeTest.php
+++ tests/Service/SnowflakeTest.php
@@ -23,4 +23,61 @@ class SnowflakeTest extends BaseTestCase
         $this->assertGreaterThan($id, $id2);
         $this->assertGreaterThan($id2, $id3);
     }
+
+    public function testParse()
+    {
+        $snowflake = new Snowflake([
+            'workerId' => 1,
+            'randomStartSequence' => false,
+        ]);
+
+        $id = $snowflake->next();
+        $id2 = $snowflake->next();
+        $result = $snowflake->parse($id);
+        $result2 = $snowflake->parse($id2);
+
+        $timestamp = (int) (microtime(true) * 1000);
+        $this->assertLessThanOrEqual($timestamp, $result['timestamp']);
+        $this->assertLessThanOrEqual($timestamp, $result2['timestamp']);
+
+        $this->assertLessThan(100, $timestamp - $result['timestamp']);
+        $this->assertLessThan(100, $timestamp - $result2['timestamp']);
+
+        $this->assertSame(1, $result['workerId']);
+        $this->assertSame(1, $result2['workerId']);
+
+        $this->assertSame(0, $result['sequence']);
+        $this->assertTrue(in_array($result2['sequence'], [0, 1], true));
+    }
+
+    public function testParseWorkerId()
+    {
+        $workerId = mt_rand(0, 1023);
+        $snowflake = new Snowflake([
+            'workerId' => $workerId,
+        ]);
+
+        $id = $snowflake->next();
+
+        $this->assertSame($workerId, $snowflake->getWorkerId());
+        $this->assertSame($snowflake->getWorkerId(), $snowflake->parse($id)['workerId']);
+    }
+
+    public function testParseShorterId()
+    {
+        $startTimestamp = time() * 1000;
+        $snowflake = new Snowflake([
+            'startTimestamp' => $startTimestamp,
+            'workerId' => 1,
+            'randomStartSequence' => false,
+        ]);
+
+        $id = $snowflake->next();
+        $this->assertTrue(in_array(strlen($id), [9, 10], true));
+
+        $result = $snowflake->parse($id);
+        $this->assertGreaterThanOrEqual($startTimestamp, $result['timestamp']);
+        $this->assertSame(1, $result['workerId']);
+        $this->assertSame(0, $result['sequence']);
+    }
 }
