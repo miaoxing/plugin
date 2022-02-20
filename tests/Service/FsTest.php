a09a439b3db79d2ca8d6d49bd1c521b296d16030
twinh <twinhuang@qq.com>
2022-02-20 18:44:49 +0800

    test(Fs): 增加 Fs 测试

---
+++ tests/Service/FsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MiaoxingTest\Plugin\Service;
+
+use Miaoxing\Plugin\Test\BaseTestCase;
+
+/**
+ * @mixin \FsMixin
+ */
+class FsTest extends BaseTestCase
+{
+    /**
+     * @dataProvider providerForGetExt
+     * @param string $path
+     * @param string $ext
+     */
+    public function testGetExt(string $path, string $ext): void
+    {
+        $this->assertSame($ext, $this->fs->getExt($path));
+    }
+
+    public function providerForGetExt(): array
+    {
+        return [
+            ['test.jpg', 'jpg'],
+            ['path/test.gif', 'gif'],
+        ];
+    }
+}
