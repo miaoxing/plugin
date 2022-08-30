1ebcbd65bbbd89a3d9c705e755738b168976a9d6
twinh <twinhuang@qq.com>
2022-08-30 23:51:36 +0800

    feat(plugin): `cache:clear` 命令允许指定缓存名称

--- src/Command/CacheClear.php
+++ src/Command/CacheClear.php
@@ -2,14 +2,35 @@
 
 namespace Miaoxing\Plugin\Command;
 
+use InvalidArgumentException;
+use Symfony\Component\Console\Input\InputArgument;
+use Wei\BaseCache;
+
 /**
  * @mixin \CacheMixin
  */
 class CacheClear extends BaseCommand
 {
+    protected function configure()
+    {
+        $this->setDescription('Clear the cache')
+            ->addArgument('driver', InputArgument::OPTIONAL, 'The service name of the cache');
+    }
+
     protected function handle()
     {
-        $this->cache->clear();
+        $driver = $this->getArgument('driver');
+
+        if ($driver) {
+            $cache = $this->wei->get($driver);
+            if (!$cache instanceof BaseCache) {
+                throw new InvalidArgumentException(sprintf('Driver "%s" is not a instance of BaseCache', $driver));
+            }
+        } else {
+            $cache = $this->cache;
+        }
+
+        $cache->clear();
         $this->suc('Cleared the cache!');
     }
 }
