5ab90847f5dfc5155a5af2e4c0b245d91681b4ec
twinh <twinhuang@qq.com>
2022-11-01 20:47:43 +0800

    feat(Upload): 增加音频扩展名和相关操作

--- src/Service/Upload.php
+++ src/Service/Upload.php
@@ -18,6 +18,19 @@ class Upload extends \Wei\Upload
     ];
 
     /**
+     * 各系统支持的音频类型的扩展名
+     *
+     * @var string[]
+     * @link https://developers.weixin.qq.com/miniprogram/dev/api/media/audio/InnerAudioContext.html#%E6%94%AF%E6%8C%81%E6%A0%BC%E5%BC%8F
+     */
+    protected $allowedAudioExts = [
+        'mp3',
+        'm4a',
+        'wav',
+        'aac',
+    ];
+
+    /**
      * 获取图片的扩展名
      *
      * @svc
@@ -45,7 +58,8 @@ class Upload extends \Wei\Upload
     protected function getAllowedExts(): array
     {
         return array_merge(
-            $this->allowedImageExts
+            $this->allowedImageExts,
+            $this->allowedAudioExts
         );
     }
 
@@ -57,5 +71,35 @@ class Upload extends \Wei\Upload
     protected function saveImage(array $options = []): \Wei\Ret
     {
         return $this->save($options + ['exts' => $this->getAllowedImageExts()]);
+    }
+
+    /**
+     * 获取音频的扩展名
+     *
+     * @svc
+     */
+    protected function getAllowedAudioExts(): array
+    {
+        return $this->allowedAudioExts;
+    }
+
+    /**
+     * 检查扩展名是否为允许的音频类型
+     *
+     * @svc
+     */
+    protected function isAllowedAudioExt(string $ext): bool
+    {
+        return in_array($ext, $this->allowedAudioExts, true);
+    }
+
+    /**
+     * 上传音频文件
+     *
+     * @svc
+     */
+    protected function saveAudio(array $options = []): \Wei\Ret
+    {
+        return $this->save($options + ['exts' => $this->getAllowedAudioExts()]);
     }
 }
