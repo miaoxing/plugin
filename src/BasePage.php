65a284705dd42fef2c22a665eba8a1a98b323e07
twinh <twinhuang@qq.com>
2022-12-15 11:51:24 +0800

    feat(plugin): `BasePage` 增加 `pageInit` 事件

--- src/BasePage.php
+++ src/BasePage.php
@@ -4,9 +4,16 @@ namespace Miaoxing\Plugin;
 
 use Wei\BaseController;
 
+/**
+ * @mixin \EventMixin
+ */
 abstract class BasePage extends BaseController
 {
+    /**
+     * Initialize the page, can be used to register middleware
+     */
     public function init()
     {
+        $this->event->trigger('pageInit', [$this]);
     }
 }
