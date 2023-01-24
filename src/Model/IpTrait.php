0b3f5a10d81653342f8d79e20e5d28a3bfb2d73f
twinh <twinhuang@qq.com>
2023-01-24 16:32:50 +0800

    feat(Model): 增加 `IpTrait`，用于为模型记录用户 IP 和端口

---
+++ src/Model/IpTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Miaoxing\Plugin\Model;
+
+/**
+ * Add user IP to model object
+ *
+ * @property-read string $createdIpColumn The column contains create user IP
+ */
+trait IpTrait
+{
+    public static function bootIpTrait(): void
+    {
+        static::onModelEvent('init', 'initIpModel');
+        static::onModelEvent('beforeCreate', 'setIpValue');
+    }
+
+    protected function getCreatedIpColumn(): string
+    {
+        return $this->createdIpColumn ?? $this->convertToPhpKey('created_ip');
+    }
+
+    /**
+     * @internal
+     */
+    protected function initIpModel(): void
+    {
+        $createdIpColumn = $this->getCreatedIpColumn();
+        $this->hidden[] = $createdIpColumn;
+        $this->guarded[] = $createdIpColumn;
+        $this->columns[$createdIpColumn]['cast'] = 'ip';
+    }
+
+    /**
+     * @internal
+     */
+    protected function setIpValue(): void
+    {
+        $this->setColumnValue($this->getCreatedIpColumn(), $this->req->getIp());
+    }
+}
