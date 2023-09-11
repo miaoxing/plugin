f515cee711b340d82bde774224b162fae6227a36
twinh <twinhuang@qq.com>
2023-09-11 14:58:32 +0800

    feat(plugin, deprecated): 废弃 `Miaoxing\Plugin\RetException`

--- src/RetException.php
+++ src/RetException.php
@@ -4,6 +4,9 @@ namespace Miaoxing\Plugin;
 
 use Wei\Ret\RetException as BaseRetException;
 
+/**
+ * @deprecated Use Wei\Ret\RetException instead
+ */
 class RetException extends BaseRetException
 {
 }
